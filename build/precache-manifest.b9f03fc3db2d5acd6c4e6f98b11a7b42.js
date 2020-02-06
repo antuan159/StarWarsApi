68d482f91404e1951b1b22537f6d0743b099015f
antuan159 <lanos159@ukr.net>
2020-02-06 09:52:38 +0200

    build deploy netlify

---
+++ build/precache-manifest.b9f03fc3db2d5acd6c4e6f98b11a7b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea4caadfbae77325754933f61deefe5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb44dbdf611abe9232bf",
+    "url": "/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "01cd4ef0e806bdd2619c",
+    "url": "/static/css/main.a683dd1f.chunk.css"
+  },
+  {
+    "revision": "cb44dbdf611abe9232bf",
+    "url": "/static/js/2.60e30734.chunk.js"
+  },
+  {
+    "revision": "01cd4ef0e806bdd2619c",
+    "url": "/static/js/main.692e8668.chunk.js"
+  },
+  {
+    "revision": "b015bde7bff135c53d70",
+    "url": "/static/js/runtime-main.af7b698f.js"
+  }
+]);
